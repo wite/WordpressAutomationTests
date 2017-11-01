5628116ca22302e32ae27803d43a096d8ae9c51f
Witold Kryszak <kryszak.witold@gmail.com>
2017-11-01 17:06:12 +0100

    Initial commit for wordpress automation tests with some basic tests

---
+++ CreatePostTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordpressAutomation;
+
+namespace WordpressTests
+{
+    [TestClass]
+    public class CreatePostTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Can_Create_A_Basic_Post()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("wkryszax").WithPassword("X011T@phbA$TQ2KFZh").Login();
+
+            NewPostPage.GoTo();
+            NewPostPage.CreatePost("This is the test post title").WithBody("Hi, this is the body").Publish();
+            NewPostPage.GoToNewPost();
+
+            Assert.AreEqual(PostPage.Title, "This is the test post title", "Title did not match new post");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            //Driver.Close();
+        }
+    }
+}
---
+++ LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordpressAutomation;
+
+namespace WordpressTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void AdminUserCanLogin()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("wkryszax").WithPassword("X011T@phbA$TQ2KFZh").Login();
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+}
